19c6ebcb951b13267d928b1745c765429b5e1a3a
Sebastian Kim <sydneyitguy@gmail.com>
2023-10-31 12:20:34 +0900

    Add more domains, update logger

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # SSL Checker
 Check SSL expiry and send notifications to a Discord channel
+
+## Cronjob
+```
+0 11 * * * cd ~/ssl-checker && node check-all-ssl.js >> ~/ssl-checker/cron.log 2>&1
+```
--- check-all-ssl.js
+++ check-all-ssl.js
@@ -4,17 +4,27 @@ const https = require('https');
 const request = require('request');
 require('dotenv').config();
 
+function format(date) {
+  return date.toLocaleString('sv-SE');
+}
+
 const LIST_OF_DOMAINS = [
   'hunt.town',
   'nomadtask.com',
   'steemhunt.com',
   'dixel.club',
   'dixelclub.com',
-  'mint.club'
+  'mint.club',
+  'hrl.sh',
+  'barkapp.co',
+  'neverlose.money',
+  'gudoks.com'
 ];
 const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL;
 const port = 443;
 const gracetime_days = 10;
+
+console.log(`---\nCheck all domains: ${format(new Date())}\n---`);
 
 LIST_OF_DOMAINS.forEach(domain => {
   const options = {
@@ -29,12 +39,12 @@ LIST_OF_DOMAINS.forEach(domain => {
       const now = new Date();
       const valid_to = new Date(cert.valid_to);
       const remaining = Math.floor((valid_to - now) / (1000 * 60 * 60 * 24)); // remaining days
-      let status = `OK - ${domain} - valid for ${remaining} days (until ${valid_to.toISOString()})`;
+      let status = `OK - ${domain} - valid for ${remaining} days (until ${format(valid_to)})`;
 
       if (remaining < 0) {
-        status = `CRITICAL - ${domain} - expired ${-remaining} days ago (on ${valid_to.toISOString()})`;
+        status = `CRITICAL - ${domain} - expired ${-remaining} days ago (on ${format(valid_to)})`;
       } else if (remaining < gracetime_days) {
-        status = `WARNING - ${domain} - expires in ${remaining} days (on ${valid_to.toISOString()})`;
+        status = `WARNING - ${domain} - expires in ${remaining} days (on ${format(valid_to)})`;
       }
 
       if (remaining < gracetime_days) { // Discord message will be sent for warning and critical status.
