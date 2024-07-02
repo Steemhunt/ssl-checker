535247b926e05bd523c50032d41b1fbb1828cd5f
Sebastian Kim <sydneyitguy@gmail.com>
2024-07-02 11:40:35 +0900

    Add check url script

---
+++ check-url.js
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+
+const https = require("https");
+const request = require("request");
+const url = require("url");
+require("dotenv").config();
+
+function format(date) {
+  return date.toLocaleString("sv-SE");
+}
+
+const LIST_OF_URLS = ["https://tip.hunt.town/allowance/stats/8151"];
+const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL;
+const port = 443;
+
+console.log(`---\nCheck all urls: ${format(new Date())}\n---`);
+
+function onError(url, msg) {
+  const status = `ERROR - ${url} - ${msg}`;
+  console.log(status);
+
+  request.post(
+    DISCORD_WEBHOOK_URL,
+    {
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ content: status }),
+    },
+    (error) => {
+      if (error) {
+        console.error(`Error sending to Discord: ${error}`);
+      }
+    }
+  );
+}
+
+LIST_OF_URLS.forEach((urlString) => {
+  const parsedUrl = new URL(urlString);
+  const options = {
+    host: parsedUrl.hostname, // Extract hostname
+    path: parsedUrl.pathname, // Extract path
+    port: port,
+    method: "GET",
+  };
+
+  const req = https.request(options, (response) => {
+    // Check URL returns 200
+    if (response.statusCode === 200) {
+      console.log(`OK - ${urlString}`);
+    } else {
+      onError(urlString, `Status code: ${response.statusCode}`);
+    }
+  });
+
+  req.on("error", (error) => {
+    onError(urlString, `Connection Error: ${error.message}`);
+  });
+
+  req.end();
+});
